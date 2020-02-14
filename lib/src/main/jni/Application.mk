5981da6728515c17cae89fd2c2b29ef993847fb7
yzk <853151446@qq.com>
2020-02-14 17:57:07 +0800

    init

---
+++ lib/src/main/jni/Application.mk
@@ -0,0 +1,6 @@
+APP_ABI := armeabi-v7a
+APP_PLATFORM := android-21
+APP_STL := gnustl_static
+APP_OPTIM := release
+VA_ROOT          := $(call my-dir)
+NDK_MODULE_PATH  := $(NDK_MODULE_PATH):$(VA_ROOT)
